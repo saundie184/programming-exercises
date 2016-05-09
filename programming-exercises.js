5af6ea13314518ced2c927143b43372dd39ceca7
Saundie Weiss <saundieweiss@gmail.com>
2016-05-08 18:01:38 -0600

    Solutions for Chocolate Feast. Need to test before submitting

--- programming-exercises.js
+++ programming-exercises.js
@@ -23,20 +23,16 @@ function teams(kids, len) {
     var W = len;
     //Array of kids in circle
     var L = kids;
-    //number of kids
-    // var num = L.length;
     var teamCount = L.length / 2;
-    // console.log(teamCount);
     var teamOne = [];
     var teamTwo = [];
-
     while (teamOne.length < teamCount) {
         // console.log(L.length);
         //if kids names length is less than rhyme length, divide and find the modulus
         if (L.length <= W) {
             if (L.length === W) {
-              // console.log('yes');
-                var index = L.length -1;
+                // console.log('yes');
+                var index = L.length - 1;
                 // console.log(index);
                 teamOne.push(L[index]);
                 L.splice(index, 1);
@@ -54,10 +50,43 @@ function teams(kids, len) {
         }
     }
     teamTwo = L;
-    // return teamOne;
     return 'Team One: ' + teamOne + '\nTeam Two: ' + teamTwo;
 }
 
-// console.log(teams(['Lilly', 'Meli', 'Toby', 'Bailey', 'Cash', 'Lexi'], 25));//Lilly, Lexi, Meli
-console.log(teams(['Lilly', 'Meli', 'Toby', 'Bailey', 'Cash', 'Lexi'], 18));//Lexi, Toby, Meli
+// console.log(teams(['Lilly', 'Meli', 'Toby', 'Bailey', 'Cash', 'Lexi'], 25)); //Lilly, Lexi, Meli
+// console.log(teams(['Lilly', 'Meli', 'Toby', 'Bailey', 'Cash', 'Lexi'], 18)); //Lexi, Toby, Meli
 // console.log(teams(['Lilly', 'Meli', 'Toby', 'Bailey', 'Cash', 'Lexi'], 6)); //team one is Lexi, Lilly, Toby
+
+//Chocolate Feast
+// Little Bob loves chocolate, and he goes to a store with $M in his pocket. The price of each
+//
+// chocolate is $C. The store offers a discount: for every X wrappers he gives to the store, he gets
+//
+// one chocolate for free. How many chocolates does Bob get to eat?
+
+function feast(cash, price, wrappers) {
+    var ChocoBought = cash / price;
+    //If he has exactly how many he needs for a free one
+    if (wrappers % ChocoBought === 0) {
+        var freeChoco = wrappers / ChocoBought;
+        return freeChoco + ChocoBought;
+        //if he doesn't have enough for a free one
+    } else if (wrappers % ChocoBought < wrappers) {
+        // var freeChoco = (wrappers / ChocoBought) - wrappers;
+        return ChocoBought;
+    } else {
+      //if he can keep saving wrappers to combine for a free one
+        var savedWrappers = 0;
+        savedWrappers += ChocoBought - wrappers;
+        // console.log(ChocoBought - wrappers);
+        // console.log(savedWrappers);
+        if (savedWrappers += (ChocoBought - wrappers) === wrappers) {
+            savedWrappers++;
+        }
+        return savedWrappers + ChocoBought;
+    }
+
+}
+console.log(feast(10, 2, 5)); //6
+console.log(feast(12, 4, 4)); //3
+console.log(feast(6, 2, 2)); //5
